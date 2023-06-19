807b2579bb54e583476a47b98636be5034e74215
504 <504@DESKTOP-FDBPKQC>
2023-06-19 18:00:10 +0900

    0619

--- frontend/public/js/1_기초문법/1_입출력.js
+++ frontend/public/js/1_기초문법/1_입출력.js
@@ -14,17 +14,17 @@
  */
 
  // 1. 출력
- console.log('안녕 Js')	// 1.문자 입력시 ' ' 사용 
- console.log("안녕 js") // 2.문자 입력시 " " 사용
- console.log( 200 )	// 3.숫자는 문자처리 x
- console.log("200")	// 4. "숫자"는 문자형식
- console.log(true)		// 5. 논리형
- console.log(20+"개")	// 6. '문자'+숫자 = 문자 (+ : 연결연산자) 
+ console.log('안녕 Js')	  // 1.문자 입력시 ' ' 사용 
+ console.log("안녕 js")     // 2.문자 입력시 " " 사용
+ console.log( 200 )	      // 3.숫자는 문자처리 x
+ console.log("200")	      // 4. "숫자"는 문자형식
+ console.log(true)		  // 5. 논리형
+ console.log(20+"개")	  // 6. '문자'+숫자 = 문자 (+ : 연결연산자) 
  
  // 2. 출력
 alert('안녕 Js')
 alert(20+20)	// 7. 숫자 + 숫자 => 숫자 (+ : 연결연산자) 40출력
-alert("20+20") //2020 출력
+alert("20+20")  //2020 출력
  
  // 3. 출력
 confirm('컨펌 Js') 
