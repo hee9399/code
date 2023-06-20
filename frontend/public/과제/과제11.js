d39405d4dda5c90da3be28bc1619a3b6368f78c8
504 <504@DESKTOP-FDBPKQC>
2023-06-20 18:04:22 +0900

    0604

---
+++ frontend/public/과제/과제11.js
@@ -0,0 +1,35 @@
+/* JS 연산지 과제1
+ 	문제11 : 정수 3개를 입력받아 오름차순 으로 출력 [759 -> 579]
+ 	문제12 : [ 가위바위보 게임 ]
+*/
+  // 과제 문제11
+let a11 = Number(prompt('정수1') )
+let b11 = Number(prompt('정수2') )
+let c11 = Number(prompt('정수3') )
+
+// 변수만들기
+let max1 = a11
+max1 = max1 < b11 ? b11 : max1
+max1 = max1 < c11 ? c11 : max1
+console.log(max1)
+
+
+let min = a11
+min = min > b11 ? b11 : min
+min = min > c11 ?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
