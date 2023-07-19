05f2a15d37f579e946a2e09b079421cbed01333c
504 <504@DESKTOP-FRGIVND>
2023-07-19 17:56:04 +0900

    17:55 정희락

--- backend/src/연습_1/연습.java
+++ backend/src/연습_1/연습.java
@@ -36,9 +36,9 @@ public class 연습 {// class start
 		 */
 		Scanner scanner = new Scanner(System.in); 
 		System.out.println("-----------------------------문제2-------------------------------");
-		System.out.print("작성자 : "); String 작성자 = scanner.next(); 
-		System.out.print("내용 : ");  String 내용 = scanner.next();
-		System.out.print("날짜 : ");  String 날짜 = scanner.next();
+		System.out.print("작성자 : ");  String 작성자 = scanner.next(); 
+		System.out.print("내용 : ");   String 내용 = scanner.next();
+		System.out.print("날짜 : ");   String 날짜 = scanner.next();
 		 
 		System.out.println("------------------방문록--------------------------------"); 
 		System.out.printf("|%4d|%4d|%10d|%4d|" , "순번" , "작성자" , "내용" , "날짜"); 
