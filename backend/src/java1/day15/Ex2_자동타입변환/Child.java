fbd1952785a3b7ef28c89dd4337d010bd38c0e08
504 <504@DESKTOP-TTN284H>
2023-08-02 21:37:40 +0900

    21:37 0802

---
+++ backend/src/java1/day15/Ex2_자동타입변환/Child.java
@@ -0,0 +1,27 @@
+package java1.day15.Ex2_자동타입변환;
+
+public class Child extends Parent {
+	// 상속 : 자식클래스 extends 부모클래스
+	
+	// 1. 필드
+	
+	// 2. 메소드
+	
+	// 3. 실행
+
+	// 오버라이딩 : 클래스내 빈공간에 커스를 두고 컨트로+스페이스바
+	
+@Override
+public void method1() {
+	System.out.println("자식클래스메소드1");
+}
+@Override
+	public void method2() {
+		System.out.println("자식클래스메소드2");
+	}	
+public void method3() {
+	System.out.println("자식클래스메소드3");
+}
+
+
+}
