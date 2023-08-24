cb13f9030e021f49529c50dc29b4985271a42548
504 <504@DESKTOP-TTN284H>
2023-08-24 18:13:26 +0900

    18:13 , 0824 , 정희락

--- backend/src/java2/day25_제네릭/Ex2/Ex2_와일드카드제네릭타입.java
+++ backend/src/java2/day25_제네릭/Ex2/Ex2_와일드카드제네릭타입.java
@@ -21,8 +21,23 @@ public class Ex2_와일드카드제네릭타입 {
 		Course.registerCourse1( new Applicant<Student>(new Student() ) );
 		Course.registerCourse1( new Applicant<MiddleStudent>(new MiddleStudent() ) );
 		
-		// ------- 코스2 : 학생만 신청 가능 
-			// Applicant< ? extlends Student > applicant
+		// ----- 코스2 : 학생만 신청 가능 
+		// Applicant< ?  extends Student > applicant 
+		// 지원서 객체 중에 Student 객체 와 Student 자손객체 만 가능 [ Person , Worker 불가능 ]
+		//Course.registerCourse2(  new Applicant<Person>( new Person() ) );
+		//Course.registerCourse2(  new Applicant<Worker>( new Worker() ) );
+		Course.registerCourse2(  new Applicant<Student>( new Student() ) );
+		Course.registerCourse2(  new Applicant<HighStudent>( new HighStudent() ) );
+		Course.registerCourse2(  new Applicant<MiddleStudent>( new MiddleStudent() ) );
+				
+		// ------ 코스3 : 직장인 및 일반인만 신청 가능 
+		// Applicant< ? super Worker> applicant
+		// 지원서 객체 중에  Worker 객체 와 Worker 부모객체 만 가능 [ Student,HighStudent,MiddleStudent 불가능 ]
+		Course.registerCourse3(  new Applicant<Person>( new Person() ) );
+		Course.registerCourse3(  new Applicant<Worker>( new Worker() ) );
+		//Course.registerCourse3(  new Applicant<Student>( new Student() ) );
+		//Course.registerCourse3(  new Applicant<HighStudent>( new HighStudent() ) );
+		//Course.registerCourse3(  new Applicant<MiddleStudent>( new MiddleStudent() ) );
 		
 	}
 }
