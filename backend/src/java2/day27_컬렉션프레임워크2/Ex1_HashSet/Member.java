70f9714d6aa1194d08f9a8853b2ba65067d0cbb0
504 <504@DESKTOP-TTN284H>
2023-08-28 17:55:47 +0900

    17:55 , 0828 , 정희락

--- backend/src/java2/day27_컬렉션프레임워크2/Ex1_HashSet/Member.java
+++ backend/src/java2/day27_컬렉션프레임워크2/Ex1_HashSet/Member.java
@@ -11,9 +11,27 @@ public class Member {
 		this.name = name;
 		this.age = age;
 	}
+
+	@Override
+	public String toString() {
+		return "Member [name=" + name + ", age=" + age + "]";
+	}
 	
+	@Override
+	public int hashCode() {
+		
+		return super.hashCode();
+	}
 	
-	
-	
+	@Override
+	public boolean equals(Object obj) {
+		//if(obj instanceof Member target) { // javaSE-16 이상 
+				if(obj instanceof Member target) { // javaSe-16 이하라면 강제형변환 필요
+					// 만약에 해당 객체가 이름과 나이가 일치하면 [ 중복 ]
+					return target.name.equals(name) && (target.age==age);
+				} else {
+					return false;
+				}
+	}
 	
 }
--- backend/src/java2/day27_컬렉션프레임워크2/z.java
+++
@@ -1,5 +0,0 @@
-package java2.day27_컬렉션프레임워크2;
-
-public class z {
-
-}
